8a06c2cc5c4d8b439a5063f70be96b117850a7c3
lukepkerrr <33287366+lukepkerrr@users.noreply.github.com>
2019-04-01 22:43:13 +0300

    Add files via upload

--- netology.2.4.py
+++ netology.2.4.py
@@ -3,7 +3,7 @@ import re
 
 from pymongo import MongoClient
 
-client = MongoClient
+client = MongoClient()
 concerts_db = client.concerts_db
 
 
@@ -14,9 +14,18 @@ def read_data(csv_file, db):
     """
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
-        reader = csv.DictReader(csvfile)
-        print(reader)
-        print(db)
+        data = []
+        reader = csv.reader(csvfile)
+        csv_list = list(reader)
+        csv_list.pop(0)
+        for line in csv_list:
+            data.append({
+                'artist': line[0],
+                'price': int(line[1]),
+                'place': line[2],
+                'date': line[3]
+            })
+        db.concerts.insert_many(data)
 
 
 
@@ -25,6 +34,9 @@ def find_cheapest(db):
     Найти самые дешевые билеты
     Документация: https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
     """
+    concerts_by_price = db.concerts.find().sort('price', 1)
+    for concert in concerts_by_price:
+        print(concert)
 
 
 def find_by_name(name, db):
@@ -33,8 +45,14 @@ def find_by_name(name, db):
     и выведите их по возрастанию цены
     """
 
-    regex = re.compile('укажите регулярное выражение для поиска')
+    regex = re.compile('.*{}.*'.format(name))
+
+    concerts_by_name = db.concerts.find({'artist': regex}).sort('price', 1)
+    for concert in concerts_by_name:
+        print(concert)
 
 
 if __name__ == '__main__':
-    read_data('artists.csv', concerts_db)
+    # read_data('artists.csv', concerts_db)
+    # find_cheapest(concerts_db)
+    find_by_name('ов', concerts_db)
